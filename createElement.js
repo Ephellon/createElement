c9c700922a7985632ba99b412a54964d394aa240
Cris @ DOSY <22254235+dosyago-coder-0@users.noreply.github.com>
2018-07-11 05:41:06 +0800

    ok

--- createElement.js
+++ createElement.js
@@ -3,16 +3,27 @@
   const nativeCreate = document.createElement.bind(document);
   document.createElement = createElement;
 
+  // just for the lulz
+  document.createElement[Symbol.toPrimitive] = () => "function createElement() { [native code] }";
+
   function createElement(name, attributes, ...children) {
+    attributes = attributes || {};
     const nativeOptions = !!attributes.is ? { is: attributes.is } : undefined;
     delete attributes.is;
+
     const element = nativeCreate(name, nativeOptions);
-    Object.entries(attributes).forEach(([name,value]) => element.setAttribute(name,value));
+
+    // content attributes vs IDL attributes have many cases
+    Object.entries(attributes).forEach(([name,value]) => name.startsWith('on') ? 
+      element[name] = value : element.setAttribute(name,value)
+    );
+
     children
       .filter( child => !(child == null || child == undefined))
       .forEach( child => element.appendChild( 
         child instanceof Node ? child : document.createTextNode(child)
-      );
+      ));
+
     return element;
   }
 }
