2b13620d417282cd1d4155fde6b4a2d56d817ef3
Ephellon <minkcbos@gmail.com>
2019-05-06 16:33:17 -0500

    Update createElement.js

--- createElement.js
+++ createElement.js
@@ -1,29 +1,65 @@
+/** Adopted from <https://github.com/crislin2046/createElement>
+* LICENSE: MIT (2018)
+*/
 "use strict";
 {
-  const nativeCreate = document.createElement.bind(document);
-  document.createElement = createElement;
+    let nativeCreate = document.createElement.bind(document);
 
-  // just for the lulz
-  document.createElement[Symbol.toPrimitive] = () => "function createElement() { [native code] }";
+    document.createElement = function createElement(TAGNAME, ATTRIBUTES = {}, ...CHILDREN) {
+        let u = v => v && v.length,
+            R = RegExp,
+            name = TAGNAME,
+            attributes = ATTRIBUTES,
+            children = CHILDREN;
 
-  function createElement(name, attributes, ...children) {
-    attributes = attributes || {};
-    const nativeOptions = !!attributes.is ? { is: attributes.is } : undefined;
-    delete attributes.is;
+        if( !u(name) )
+            throw TypeError(`TAGNAME cannot be ${ (name === '')? 'empty': name }`);
 
-    const element = nativeCreate(name, nativeOptions);
+        let options = attributes.is === true? { is: true }: null;
 
-    // content attributes vs IDL attributes have many cases
-    Object.entries(attributes).forEach(([name,value]) => name.startsWith('on') ? 
-      element[name] = value : element.setAttribute(name,value)
-    );
+        delete attributes.is;
 
-    children
-      .filter( child => !(child == null || child == undefined))
-      .forEach( child => element.appendChild( 
-        child instanceof Node ? child : document.createTextNode(child)
-      ));
+        name = name.split(/([#\.][^#\.\[\]]+)/).filter( u );
 
-    return element;
-  }
+        if(name.length <= 1)
+            name = name[0].split(/^([^\[\]]+)(\[.+\])/).filter( u );
+
+        if(name.length > 1)
+            for(let n = name, i = 1, l = n.length, t, v; i < l; i++)
+                if((v = n[i].slice(1, n[i].length)) && (t = n[i][0]) == '#')
+                    attributes.id = v;
+                else if(t == '.')
+                    attributes.classList = [].slice.call(attributes.classList || []).concat(v);
+                else if(/\[(.+)\]/.test(n[i]))
+                    R.$1.split('][').forEach(N => attributes[(N = N.split('=', 2))[0]] = N[1] || '');
+        name = name[0];
+
+        let element = document.createElement(name, options);
+
+        if(attributes.classList instanceof Array)
+            attributes.classList = attributes.classList.join(' ');
+
+        Object.entries(attributes).forEach(
+            ([name, value]) => (/^(on|(?:(?:inner|outer)(?:HTML|Text)|textContent|class(?:List|Name)|value)$)/.test(name))?
+                element[name] = value:
+            element.setAttribute(name, value)
+        );
+
+        children
+            .filter( child => child !== undefined && child !== null )
+            .forEach(
+                child =>
+                    child instanceof Element?
+                        element.append(child):
+                    child instanceof Node?
+                        element.appendChild(child):
+                    element.appendChild(
+                        parent.createTextNode(child)
+                    )
+            );
+
+        return element;
+    };
+
+    document.createElement[Symbol.toPrimitive] = () => 'function createElement() { [native code] }';
 }
